7f0b527789dc1dca428998f53270dc309406f5e1
mkaletaa <kaleta222@gmail.com>
2022-10-06 21:40:24 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Topnav from './components/Topnav';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<Topnav></Topnav>
+        <p>
+          Edit <code className="font-bold text-3xl">src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Topnav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Topnav() {
+  return (
+    <div className='w-screen top-0' style={{background: 'red', position: 'fixed'}} >Topnav
+    <FontAwesomeIcon icon="fa-solid fa-house" />
+    </div>
+  )
+}
