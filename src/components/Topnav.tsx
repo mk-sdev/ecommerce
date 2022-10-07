a5b32352fc8c6f082b8f493aca8c78d10f1d3902
mkaletaa <kaleta222@gmail.com>
2022-10-07 11:01:51 +0200

    before image

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHouse} from '@fortawesome/free-solid-svg-icons'
+import {faMagnifyingGlass}  from '@fortawesome/free-solid-svg-icons'
+import {faUser}  from '@fortawesome/free-solid-svg-icons'
+import {faBasketShopping}  from '@fortawesome/free-solid-svg-icons'
+// import logo from "logo.png"
 
 export default function Topnav() {
+  
   return (
-    <div className='w-screen top-0' style={{background: 'red', position: 'fixed'}} >Topnav
+    <div className='w-screen top-0' style={{background: 'red', position: 'fixed'}} >
+      {/* <img src={'./logo.png'} alt="" /> */}
     <FontAwesomeIcon icon={ faHouse} />
+    <FontAwesomeIcon icon={faMagnifyingGlass} />
+    <FontAwesomeIcon icon={faUser} />
+    <FontAwesomeIcon icon={faBasketShopping} />
     </div>
   )
 }
