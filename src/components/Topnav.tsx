9ffea645498c8c43e5b9003b94dc38ac4262c831
mkaletaa <kaleta222@gmail.com>
2022-10-07 15:24:11 +0200

    done except searchbar

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -13,7 +13,7 @@ const logo = require("../images/logo.png")
 export default function Topnav() {
   
   const [search, setSearch] = useState('-100px')
-  const [menu, setMenu] = useState('250px')
+  const [menu, setMenu] = useState(true)
   const searchBar = React.useRef<HTMLInputElement>(null);
 
 
@@ -23,11 +23,11 @@ export default function Topnav() {
     <div id='topBar' className='w-screen top-0'>
     <img id='topBarLogo' src={String(logo)} alt="" width='150px' className='hover:cursor-pointer' />
 
-    <div id="topBarLinks" className='text-black font-semibold' style={{left: menu}}>
+    <div id="topBarLinks" className={`text-black font-semibold ${!menu && 'open'}`}>
       <a href="#" >NEW</a>
       <a href="#">BESTSELLERS</a>
       <a href="#">DISCOUNTS</a>
-    <FontAwesomeIcon id='xMark' className='text-red-500 hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu('100%')} />
+    <FontAwesomeIcon id='xMark' className='text-red-500 hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu(true)} />
     </div>
 
     <div id="topBarIcons" className='w-4/12 '>
@@ -39,7 +39,7 @@ export default function Topnav() {
     <span id="notification" className='bg-red-500 rounded-full bottom-0 left-0'></span>
     <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faBasketShopping} />
     </div>
-    <FontAwesomeIcon id='hamburger' className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faBars} onClick={e=>{setMenu('0'); setSearch('-100px')}} />
+    <FontAwesomeIcon id='hamburger' className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faBars} onClick={e=>{setMenu(false); setSearch('-100px')}} />
     </div>
 
 
@@ -49,6 +49,9 @@ export default function Topnav() {
       style={{top: search}}>
         <input id='searchInput' className='rounded-l-md ' placeholder='type any title' type='text' ref={searchBar} ></input>
         <FontAwesomeIcon className=' text-dimgray hover:cursor-pointer text-md md:text-2xl' icon={faMagnifyingGlass} />
+        
+        <FontAwesomeIcon style={{position: 'absolute', top: '-100%', right: '0px'}} className='text-red-500 text-3xl hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu(true)} />
+
       </div>
 
     </>
