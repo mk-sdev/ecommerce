3b3ba6307905ced6d05c43bfbafa06365c1a5997
mkaletaa <kaleta222@gmail.com>
2022-10-07 11:24:03 +0200

    logo added

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -4,17 +4,19 @@ import {faHouse} from '@fortawesome/free-solid-svg-icons'
 import {faMagnifyingGlass}  from '@fortawesome/free-solid-svg-icons'
 import {faUser}  from '@fortawesome/free-solid-svg-icons'
 import {faBasketShopping}  from '@fortawesome/free-solid-svg-icons'
-// import logo from "logo.png"
+const logo = require("./logo.png")
+
 
 export default function Topnav() {
   
   return (
-    <div className='w-screen top-0' style={{background: 'red', position: 'fixed'}} >
-      {/* <img src={'./logo.png'} alt="" /> */}
-    <FontAwesomeIcon icon={ faHouse} />
-    <FontAwesomeIcon icon={faMagnifyingGlass} />
-    <FontAwesomeIcon icon={faUser} />
-    <FontAwesomeIcon icon={faBasketShopping} />
+    <div className='w-screen top-0' style={{background: 'beige', position: 'fixed'}} >
+    <img src={String(logo)} alt="" width='150px' className='hover:cursor-pointer' />
+
+    <FontAwesomeIcon className='text-black hover:cursor-pointer' icon={ faHouse} />
+    <FontAwesomeIcon className='text-black hover:cursor-pointer' icon={faMagnifyingGlass} />
+    <FontAwesomeIcon className='text-black hover:cursor-pointer' icon={faUser} />
+    <FontAwesomeIcon className='text-black hover:cursor-pointer' icon={faBasketShopping} />
     </div>
   )
 }
