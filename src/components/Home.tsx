f9a0d219fad911b04021ff4dc2adf8faee7ddf19
mkaletaa <kaleta222@gmail.com>
2022-10-07 19:11:15 +0200

    routes added

---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='component' style={{background: 'red',  height: '100vh'}}>Home</div>
+  )
+}
--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -7,8 +7,8 @@ import {faBasketShopping}  from '@fortawesome/free-solid-svg-icons'
 import {faSquareXmark}  from '@fortawesome/free-solid-svg-icons'
 import {faBars}  from '@fortawesome/free-solid-svg-icons'
 import '../styles/topnav.css'
+import {Link} from 'react-router-dom'
 const logo = require("../images/logo.png")
-
 
 export default function Topnav() {
   
@@ -16,43 +16,50 @@ export default function Topnav() {
   const [menu, setMenu] = useState(true)
   const searchBar = React.useRef<HTMLInputElement>(null);
 
-
-
   return (
     <>
+
     <div id='topBar' className='w-screen top-0'>
     <img id='topBarLogo' src={String(logo)} alt="" width='150px' className='hover:cursor-pointer' />
 
     <div id="topBarLinks" className={`text-black font-semibold ${!menu && 'open'}`}>
-      <a href="#" >NEW</a>
-      <a href="#">BESTSELLERS</a>
-      <a href="#">DISCOUNTS</a>
+      <Link to='/books/new'>NEW</Link>
+      <Link to='/books/bestsellers'>BESTSELLERS</Link>
+      <Link to='/books/discounts'>DISCOUNTS</Link>
     <FontAwesomeIcon id='xMark' className='text-red-500 hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu(true)} />
     </div>
 
-    <div id="topBarIcons" className='w-4/12 '>
-    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={ faHouse} />
-    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faMagnifyingGlass} 
+
+  <div id="topBarIcons">
+    <Link to='/'>
+    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-2xl' icon={ faHouse} />
+    </Link>
+
+    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-2xl' icon={faMagnifyingGlass} 
     onClick={e=>{ searchBar.current?.focus(); search==='100px' ? setSearch('-100px') : setSearch('100px')}} />
-    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faUser} />
+
+    <Link to='/profile'>
+    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-2xl' icon={faUser} />
+    </Link>
+
     <div id="basket">
+    <Link to='/cart'>
     <span id="notification" className='bg-red-500 rounded-full bottom-0 left-0'></span>
-    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faBasketShopping} />
-    </div>
-    <FontAwesomeIcon id='hamburger' className='topBarIcons text-black hover:cursor-pointer text-3xl' icon={faBars} onClick={e=>{setMenu(false); setSearch('-100px')}} />
-    </div>
-
-
+    <FontAwesomeIcon className='topBarIcons text-black hover:cursor-pointer text-2xl' icon={faBasketShopping} />
+    </Link>
     </div>
 
-      <div id="searchBar" className='rounded-md text-black font-semibold' 
+    <FontAwesomeIcon id='hamburger' className='topBarIcons text-black hover:cursor-pointer text-2xl' icon={faBars} onClick={e=>{setMenu(false); setSearch('-100px')}} />
+    </div>
+  </div>
+
+    <div id="searchBar" className='rounded-md text-black font-semibold' 
       style={{top: search}}>
-        <input id='searchInput' className='rounded-l-md ' placeholder='type any title' type='text' ref={searchBar} ></input>
-        <FontAwesomeIcon className=' text-dimgray hover:cursor-pointer text-md md:text-2xl' icon={faMagnifyingGlass} />
-        
-        <FontAwesomeIcon style={{position: 'absolute', top: '-100%', right: '0px'}} className='text-red-500 text-3xl hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu(true)} />
-
-      </div>
+      <input id='searchInput' className='rounded-l-md ' placeholder='type any title' type='text' ref={searchBar} ></input>
+      <FontAwesomeIcon id='glass' className=' text-dimgray hover:cursor-pointer text-md md:text-xl' icon={faMagnifyingGlass} onClick={e=>setSearch('-100px')}  />
+      
+      {/* <FontAwesomeIcon style={{position: 'absolute', top: '-100%', right: '0px'}} className='text-red-500 text-3xl hover:text-red-400' icon={faSquareXmark} onClick={e=>setMenu(true)} /> */}
+    </div>
 
     </>
   )
