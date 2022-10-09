e09538ca574e521a3d222751ccd79ebc742f9e78
mkaletaa <kaleta222@gmail.com>
2022-10-09 11:13:59 +0200

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import '../styles/footer.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-// import {faSquareInstagram}  from '@fortawesome/free-solid-svg-icons'
+import {faInstagram, faPinterest, faTwitter}  from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
   return (
     <>
-    <div id='footer'>Footer</div>
-    {/* <FontAwesomeIcon icon={faSquareInstagram}/> */}
+    <div id='footer'>
+
+    <div id="footerLinks">
+      <a href="">STATUT</a>·
+      <a href="">PAYMENT</a>·
+      <a href="">CONTACT</a>·
+      <a href="">DELIVERY</a>·
+      <a href="">FAQ</a>
+    </div>
+
+    <div id="socials">
+    <a href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} className='footerIcons text-3xl' /></a>
+    <a href="https://pl.pinterest.com/"><FontAwesomeIcon icon={faPinterest} className='footerIcons text-3xl' /></a>
+    <a href="https://twitter.com/home"><FontAwesomeIcon icon={faTwitter} className='footerIcons text-3xl' /></a>
+    </div>
+
+    <p id='copyright'>Copyright © 2021 BookStore.</p>
+    </div>
     </>
   )
 }
